c4f9268bafd83f8c22e36d3a9db27e315d6d302c
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2019-05-10 00:48:33 +0200

    done

---
+++ lessonJs5B/js/main.js
@@ -0,0 +1,14 @@
+let button = document.querySelector('#start'),
+    collection = document.querySelectorAll("div[class*='-value']"),
+    input = document.querySelectorAll('.expenses-item'),
+    butAppr = document.getElementsByTagName("button")[0],
+    butAppr2 = document.getElementsByTagName("button")[1],
+    butCalc = document.getElementsByTagName("button")[2],
+    costs = document.querySelectorAll(".optionalexpenses-item"),
+    income = document.querySelector('.choose-income-label'),
+    savings = document.querySelector('.checksavings'),
+    sum = document.querySelector('.choose-sum'),
+    percent = document.querySelector('.choose-percent'),
+    time = document.querySelector('.time-data');
+
+
